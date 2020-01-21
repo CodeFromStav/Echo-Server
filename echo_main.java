3a65528750b36e417ff3e5def60d11f32353b443
Stavros <stav.triantis@gmail.com>
2020-01-21 12:21:19 -0700

    wrote main function, using Java ServerSocket API as reference

--- echo_main.java
+++ echo_main.java
@@ -1,13 +1,47 @@
+import java.net.Socket;
+import java.net.ServerSocket;
+
 class EchoServer
 {
     
 
-/* @param args
-*/
-public static void main (String[] args)
-{
+    /* @param args
+    */
+    public static void main (String[] args) throws Exception
+    {
 
-}
+        String data;
+
+        int port = 8080;
+
+        //creates server socket object
+        ServerSocket serverSocket = new ServerSocket( port ); 
+        System.err.println( "Server started on port " + port );
+
+        while( true )
+        {
+            //accepts incoming request to socket using hostname,port,localport
+            Socket clientSocket = serverSocket.accept();
+            System.err.println( "Accepted connection from client" );
+
+            //opens I/O streams
+            Input in = new Input ( clientSocket );
+            Output out = new Output ( clientSocket );
+
+            //waits for data and reads it until connection stops
+            //readLine() blocks until the server recieved a new line from client
+            while ( ( data = in.readLine() ) != null )
+            {
+                System.out.println( data );
+            }
+
+            System.err.println( "Closing connection with client" );
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
+
+    }
 
 }
 
--- echo_thread.java
+++ echo_thread.java
@@ -1,4 +1,24 @@
 /*
 The class name for the thread talking to the client is EchoThread.
  It needs to implement Runnable (and does not subclass Thread!) 
+
+The class name for the thread talking to the client is EchoThread. It needs to implement Runnable (and does not subclass Thread!)
+
+The variable holding a reference to the input stream pouring in characters from the client needs to be called fromClient
+
+The variable holding a reference to the output stream that is sending characters back to the client needs to be called toClient
+
+The variable that holds the character that was currently read from the client needs to be called charFromClient
 */
+
+/*
+public void EchoThread()
+{
+    String fromClient;
+    String toClient;
+    String charFromClient;
+
+
+}
+*/
+
