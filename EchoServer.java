2240e743bd12fe4b7e648230a624287f47381903
John Vergona <ber98@nau.edu>
2020-01-22 11:54:17 -0700

    Add files via upload

---
+++ EchoServer.java
@@ -0,0 +1,21 @@
+import java.net.*;
+import java.io.*;
+
+public class EchoServer {
+  public static void main(String[] args) throws Exception {
+    try{
+      ServerSocket server=new ServerSocket(8080);
+      int counter=0;
+      System.out.println("Server Started ....");
+      while(true){
+        counter++;
+        Socket serverClient=server.accept();  //server accept the client connection request
+        System.out.println(" >> " + "Client No:" + counter + " started!");
+        EchoThread et = new EchoThread(serverClient,counter); //send  the request to a separate thread
+        et.start();
+      }
+    } catch(Exception e){
+      System.out.println(e);
+    }
+  }
+}
---
+++ EchoThread.java
@@ -0,0 +1,40 @@
+import java.net.*;
+import java.io.*;
+public class EchoThread implements Runnable {
+  Thread currThread;
+  Socket clientSock;
+  int clientNu;
+  int squre;
+  EchoThread(Socket inSocket,int counter){
+    clientSock = inSocket;
+    clientNu=counter;
+  }
+  public void run(){
+    try{
+      DataInputStream inStream = new DataInputStream(clientSock.getInputStream());
+      DataOutputStream outStream = new DataOutputStream(clientSock.getOutputStream());
+      String clientMSG="", serverMSG="";
+      while(!clientMSG.equals("bye")){
+        clientMSG=inStream.readUTF();
+        System.out.println("From Client-" +clientNu+ ": Number is :"+clientMSG);
+        squre = Integer.parseInt(clientMSG) * Integer.parseInt(clientMSG);
+        serverMSG="From Server to Client-" + clientNu + " Square of " + clientMSG + " is " +squre;
+        outStream.writeUTF(serverMSG);
+        outStream.flush();
+      }
+      inStream.close();
+      outStream.close();
+      clientSock.close();
+    }catch(IOException e){
+      System.out.println(e);
+    }finally{
+      System.out.println("Client -" + clientNu + " exit!! ");
+    }
+  }
+  public void start () {
+      if (currThread == null) {
+        currThread = new Thread(this);
+        currThread.start();
+      }
+   }
+}
